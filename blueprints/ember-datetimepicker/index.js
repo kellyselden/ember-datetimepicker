fa169d65ad0c2291bbfc5dd06f0f067144cd9d13
Kelly Selden <kellyselden@gmail.com>
2015-10-22 20:13:01 -0400

    fixing install

--- blueprints/ember-datetimepicker/index.js
+++ blueprints/ember-datetimepicker/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  description: '',
+  normalizeEntityName: function() {},
 
   afterInstall: function(options) {
     return this.addBowerPackageToProject('datetimepicker', '^2.0.0').then(function() {
       return this.addAddonToProject({ name: 'ember-cli-moment-shim', target: '0.6.2' });
-    });
+    }.bind(this));
   }
 };
