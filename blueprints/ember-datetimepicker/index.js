08bfbf6f0e315e0a9c6ad38a5adc50cc55e7d9a9
Kelly Selden <kellyselden@gmail.com>
2016-03-05 12:54:27 -0500

    locking 2.4.5

--- blueprints/ember-datetimepicker/index.js
+++ blueprints/ember-datetimepicker/index.js
@@ -2,7 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('datetimepicker', '^2.0.0').then(function() {
+    return this.addBowerPackageToProject('datetimepicker', '2.4.5').then(function() {
       return this.addAddonToProject({ name: 'ember-cli-moment-shim', target: '0.6.2' });
     }.bind(this));
   }
