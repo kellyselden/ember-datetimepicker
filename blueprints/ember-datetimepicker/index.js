a4da3f6a931a25f971289b6b536c6c6384b279fa
Kelly Selden <kellyselden@gmail.com>
2016-03-05 13:29:15 -0500

    update ember-cli-moment-shim to ^1.0.0

--- blueprints/ember-datetimepicker/index.js
+++ blueprints/ember-datetimepicker/index.js
@@ -3,7 +3,7 @@ module.exports = {
 
   afterInstall: function(options) {
     return this.addBowerPackageToProject('datetimepicker', '2.4.5').then(function() {
-      return this.addAddonToProject({ name: 'ember-cli-moment-shim', target: '0.6.2' });
+      return this.addAddonToProject({ name: 'ember-cli-moment-shim', target: '^1.0.0' });
     }.bind(this));
   }
 };
