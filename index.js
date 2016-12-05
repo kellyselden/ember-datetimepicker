74d117228d430361fe2992aee6ffd4ecc14174c0
Kelly Selden <kellyselden@gmail.com>
2016-12-04 20:27:40 -0800

    fix funnel

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 /* jshint node: true */
 'use strict';
 
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
-
 module.exports = {
   name: 'ember-datetimepicker',
-
-  treeForVendor: function(tree) {
-    var newTree = new Funnel('node_modules/jquery-datetimepicker', {
-      files: [
-        'jquery.datetimepicker.css',
-        'build/jquery.datetimepicker.full.js'
-      ],
-      destDir: 'jquery-datetimepicker'
-    });
-
-    return mergeTrees([newTree, tree]);
-  },
-
-  included: function(app) {
-    this._super.included(app);
-
-    app.import('vendor/jquery-datetimepicker/jquery.datetimepicker.css');
-    app.import('vendor/jquery-datetimepicker/build/jquery.datetimepicker.full.js');
+  options: {
+    nodeAssets: {
+      'jquery-datetimepicker': {
+        import: [
+          'jquery.datetimepicker.css',
+          'build/jquery.datetimepicker.full.js'
+        ]
+      }
+    }
   }
 };
