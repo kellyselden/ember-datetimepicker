284623a4f42e6a23ee96a7505951191f80545d7d
Kelly Selden <kellyselden@gmail.com>
2017-12-15 11:43:56 -0800

    use require to read package.json

--- blueprints/ember-datetimepicker/index.js
+++ blueprints/ember-datetimepicker/index.js
@@ -1,14 +1,11 @@
 /* eslint-env node */
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
+    let pkg = require('../../package.json');
 
     return this.addPackageToProject('jquery-datetimepicker', pkg.devDependencies['jquery-datetimepicker']).then(() => {
       return this.addAddonToProject({ name: 'ember-cli-moment-shim', target: pkg.devDependencies['ember-cli-moment-shim'] });
