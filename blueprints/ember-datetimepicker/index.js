9b27bcd51747a5877de0feb2942a449a247e08af
Kelly Selden <kellyselden@gmail.com>
2017-12-17 11:22:31 -0800

    Revert "use require to read package.json"
    This reverts commit 284623a4f42e6a23ee96a7505951191f80545d7d.

--- blueprints/ember-datetimepicker/index.js
+++ blueprints/ember-datetimepicker/index.js
@@ -1,11 +1,14 @@
 /* eslint-env node */
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    let pkg = require('../../package.json');
+    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
 
     return this.addPackageToProject('jquery-datetimepicker', pkg.devDependencies['jquery-datetimepicker']).then(() => {
       return this.addAddonToProject({ name: 'ember-cli-moment-shim', target: pkg.devDependencies['ember-cli-moment-shim'] });
